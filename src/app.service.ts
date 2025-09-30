cc14c3fb6ddc164fb18fddd2673f5f5cf7784098
Sahan Kodituwakku <sahan.crkodituwakku@gmail.com>
2025-09-30 17:04:36 +0530

    temp: test for github webhook setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from Roqore!';
+    return 'Hello from Roqore to You!';
   }
 }
